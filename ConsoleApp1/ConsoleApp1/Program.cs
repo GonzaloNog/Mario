4311be6a905f9283146db89617b0e6c54fd940f0
Nog <63429016+GonzaloNog@users.noreply.github.com>
2023-03-28 12:03:04 -0300

    Clase uno sintaxis

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 0;
+            //float b = 1.0f;
+            //double c = 1.0f;
+            //bool d = false;
+            //string e = "HOLA";
+            //char f = 'a';
+
+            // Comentarios
+
+            /*
+             Comentario Largo
+                1
+                2
+                3
+            */
+            Console.WriteLine(a);
+        }
+    }
+}
