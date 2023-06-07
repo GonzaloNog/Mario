42a3c936fce60b19cf38e42cb9d7e16bac06d3bb
gzeissSRL <63429016+GonzaloNog@users.noreply.github.com>
2023-06-07 20:40:16 -0300

    fix

--- ConsoleApp1/ConsoleApp2/Program.cs
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -6,7 +6,41 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            holamundo();
+            bool vivo = false;
+            if (vivo)
+            {
+                Console.WriteLine("Correcto!!!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto!!!");
+            }
+            int a = 0;
+            while(a < 10)
+            {
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine(a * 10);    
+            }
         }
+        static void holamundo()
+        {
+            Console.WriteLine("Hello World!");
+            suma();
+            //suma(4, 2);
+        }
+        static void suma(int a, int b, int c = 1)
+        {
+            Console.WriteLine(a + b + c);
+        }
+        static void suma(int a, int b, int c, int d)
+        {
+            Console.WriteLine(a + b + c + d);
+        }
+        static void suma()
+        {
+            Console.WriteLine("Error: no cargaste ningun numero");
+        }
+
     }
 }
